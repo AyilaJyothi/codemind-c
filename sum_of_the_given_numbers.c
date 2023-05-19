cb62028e208976b6084fbe021f7bbae8780c2dd8
AyilaJyothi <119840130+AyilaJyothi@users.noreply.github.com>
2023-05-20 00:52:43 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     for(i=0;i<a;i++)     {         int m,n,s;         scanf("%d%d",&m,&n);         s=m+n;         printf("%d ",s);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        int m,n,s;
+        scanf("%d%d",&m,&n);
+        s=m+n;
+        printf("%d
+",s);
+    }
+}
